e15f0d47efb11b72ef090c5affe9b4d5773d72a5
Simran-Sahni <sahnisimran58@gmail.com>
2020-07-05 19:46:16 +0530

    combining all components in 1 file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,112 @@
 import React, { Component } from "react";
-import Grid from "./Grid";
+import PropTypes from 'prop-types';
+
+
+      function NavBar(){
+          return (
+           <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <a id="refreshButton" className="navbar-brand" href="#">
+                        <i className="fa fa-rocket"></i>Conquer Mars!</a
+                    >
+                </div>
+                <ul className="nav navbar-nav">
+                    <li className="dropdown">
+                        <button
+                            className="btn dropdown-toggle"
+                            type="button"
+                            data-toggle="dropdown"
+                        >
+                            Algorithms <span className="caret"></span>
+                        </button>
+                        <ul className="dropdown-menu">
+                            <li id="startButtonDijkstra">
+                                <a href="#">Dijkstra's Algorithm</a>
+                            </li>
+                            <li id="startButtonAStar2"><a href="#">A* Search</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <button className="btn">
+                            Randomize
+                        </button>
+                    </li>
+                    <li>
+                        <button className="btn">
+                            Clear Walls
+                        </button>
+                    </li>
+                    <li>
+                        <button className="btn">
+                            Clear Path
+                        </button>
+                    </li>
+                    <li className="dropdown">
+                        <button
+                            className="btn dropdown-toggle"
+                            type="button"
+                            data-toggle="dropdown"
+                        >
+                            Speed <span className="caret"></span>
+                        </button>
+                        <ul className="dropdown-menu">
+                            <li><a href="#">Fast</a></li>
+                            <li><a href="#">Medium</a></li>
+                            <li><a href="#">Slow</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+       );
+    }
+
+
+
+function Grid(props) {
+    const height = props.height; const width = props.width;
+    const gridList = [];
+    for (let i = 0; i < height; i++) {
+        const rowList = [];
+        for (let j = 0; j < width; j++) {
+            rowList.push(
+                <div
+                    key={i + j}
+                    style={{
+                        width: '30px',
+                        height: '30px',
+                        border: '1px solid black',
+                        WebkitUserSelect: 'none',
+                    }}
+                >
+
+                </div>,
+            );
+        }
+        gridList.push(rowList);
+    }
+    return (
+        <div className="p-4">
+            {' '}
+            {gridList.map((object, index) => {
+                return (
+                    <div className="row justify-content-center " key = {index}>
+                        {' '}
+                        {object}
+                        {' '}
+                    </div>
+                );
+            })}{' '}
+        </div>
+    );
+}
+Grid.propTypes = {
+    height: PropTypes.number,
+    width: PropTypes.number,
+};
+
+//ReactDOM.render(<Grid />,document.getElementById("root"));
 class App extends Component{
   state = {
     height: 20,
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavBar from './App';
+import Grid from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<NavBar />, document.getElementById('nav'));
+ReactDOM.render(<App />,  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
