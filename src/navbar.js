ca31bf737c59dbb463e1d790de3fc4363a93fc04
Radhesh Sarma <radheshsarma29@gmail.com>
2020-07-06 21:50:01 +0530

    added algo selector

--- src/navbar.js
+++ src/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Navbar, Nav, Form, FormControl, Button, NavDropdown} from 'react-bootstrap';
 
@@ -9,45 +9,60 @@ import {Navbar, Nav, Form, FormControl, Button, NavDropdown} from 'react-bootstr
  * @return {*}
  * @constructor
  */
-function Navbarr(props) {
-  return (
-    <Navbar expand="lg">
-      <Navbar.Brand >
-        <img
-          src="/public/rocket1.svg"
-          alt=""
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
-        Conquer Mars!
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
 
-        <Nav className="mr-auto">
-          <NavDropdown title="Algorithms" id="basic-nav-dropdown">
-            <NavDropdown.Item>A*</NavDropdown.Item>
-            <NavDropdown.Item>Best First Search</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item >Dijkstra</NavDropdown.Item>
-            <NavDropdown.Item >BFS</NavDropdown.Item>
-            <NavDropdown.Item >DFS</NavDropdown.Item>
-          </NavDropdown>
-          <NavDropdown title="Speed" id="basic-nav-dropdown">
-            <NavDropdown.Item onClick = {() => props.newSpeed(750)}>Slow</NavDropdown.Item>
-            <NavDropdown.Item onClick = {() => props.newSpeed(350)}>Medium</NavDropdown.Item>
-            <NavDropdown.Item onClick = {() => props.newSpeed(1)}>Fast</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link>Visualize</Nav.Link>
-        </Nav>
-        <Nav.Link onClick ={props.randomize}>Randomize</Nav.Link>
-        <Nav.Link onClick ={props.clearWalls}>Clear Grid</Nav.Link>
-        <Nav.Link >Clear Path</Nav.Link>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+class Navbarr extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedAlgo: '',
+    };
+  }
+  changeAlgo = (newAlgo) => {
+    console.log(this.state.selectedAlgo);
+    this.setState({selectedAlgo: newAlgo});
+
+  }
+  render() {
+    return (
+      <Navbar expand="lg">
+        <Navbar.Brand >
+          <img
+            src="/public/rocket1.svg"
+            alt=""
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+            Conquer Mars!
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+
+          <Nav className="mr-auto">
+            <NavDropdown title="Algorithms" id="basic-nav-dropdown">
+              <NavDropdown.Item onClick = {() => this.changeAlgo("A*")}>A*</NavDropdown.Item>
+              <NavDropdown.Item onClick = {() => this.changeAlgo("Best First Search")}>Best First Search</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick = {() => this.changeAlgo("Dijkstra")}>Dijkstra</NavDropdown.Item>
+              <NavDropdown.Item onClick = {() => this.changeAlgo("BFS")}>BFS</NavDropdown.Item>
+              <NavDropdown.Item onClick = {() => this.changeAlgo("DFS")}>DFS</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Speed" id="basic-nav-dropdown">
+              <NavDropdown.Item onClick = {() => this.props.newSpeed(750)}>Slow</NavDropdown.Item>
+              <NavDropdown.Item onClick = {() => this.props.newSpeed(350)}>Medium</NavDropdown.Item>
+              <NavDropdown.Item onClick = {() => this.props.newSpeed(1)}>Fast</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link>Visualize {this.state.selectedAlgo}</Nav.Link>
+          </Nav>
+          <Nav.Link onClick ={this.props.randomize}>Randomize</Nav.Link>
+          <Nav.Link onClick ={this.props.clearWalls}>Clear Grid</Nav.Link>
+          <Nav.Link >Clear Path</Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
+
 Navbarr.propTypes = {
   clearWalls: PropTypes.func,
   randomize: PropTypes.func,
