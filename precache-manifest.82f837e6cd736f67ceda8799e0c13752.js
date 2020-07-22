be61e18639da0c3233a53f1a0af9d2ad11e379ef
Radhesh Sarma <radheshsarma29@gmail.com>
2020-07-23 00:48:32 +0530

    Updates

---
+++ precache-manifest.82f837e6cd736f67ceda8799e0c13752.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bdfaea5c4b12166c478cf108283d60e",
+    "url": "/mars-colonization-project/index.html"
+  },
+  {
+    "revision": "a7a45d0861d493a27213",
+    "url": "/mars-colonization-project/static/css/main.e38bf9fb.chunk.css"
+  },
+  {
+    "revision": "ae7bd8bb06d9b09db066",
+    "url": "/mars-colonization-project/static/js/2.e96ec866.chunk.js"
+  },
+  {
+    "revision": "17d4ab0f68f3f6908b6e95cf7c65f310",
+    "url": "/mars-colonization-project/static/js/2.e96ec866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a45d0861d493a27213",
+    "url": "/mars-colonization-project/static/js/main.de7bc4f9.chunk.js"
+  },
+  {
+    "revision": "cc55670bc2b2ed2c3d8a",
+    "url": "/mars-colonization-project/static/js/runtime-main.4ea65d4a.js"
+  },
+  {
+    "revision": "ee7b2b54ef7a66d28ac1698bfc4d78d3",
+    "url": "/mars-colonization-project/static/media/LANDING.ee7b2b54.png"
+  },
+  {
+    "revision": "92e5dcd4aec1d077f4024b3ee2c3f676",
+    "url": "/mars-colonization-project/static/media/bg.92e5dcd4.png"
+  },
+  {
+    "revision": "59452d47c4af0023fc3f80a3ee3b9012",
+    "url": "/mars-colonization-project/static/media/mars-rover.59452d47.svg"
+  },
+  {
+    "revision": "7a8733c3a3dca214cda82f97c1b02cce",
+    "url": "/mars-colonization-project/static/media/mars1.7a8733c3.jpg"
+  },
+  {
+    "revision": "7bf85db4734422e58871d4d1721918f7",
+    "url": "/mars-colonization-project/static/media/mars2.7bf85db4.jpg"
+  },
+  {
+    "revision": "6a21f55bd4ae413fa6ab72f905c7fab0",
+    "url": "/mars-colonization-project/static/media/mars3.6a21f55b.jpg"
+  },
+  {
+    "revision": "17d38c2f917ff0640291d927d88597ad",
+    "url": "/mars-colonization-project/static/media/mars4.17d38c2f.png"
+  },
+  {
+    "revision": "548733ff59ce4c758cc314499b052536",
+    "url": "/mars-colonization-project/static/media/mars5.548733ff.jpg"
+  },
+  {
+    "revision": "97edad93af2427cab6696a0a77fb7f44",
+    "url": "/mars-colonization-project/static/media/mars6.97edad93.jpg"
+  },
+  {
+    "revision": "f9354f0a385462a105fd49652877a0c9",
+    "url": "/mars-colonization-project/static/media/mars7.f9354f0a.jpg"
+  },
+  {
+    "revision": "474640cca8b43715289b64273f774435",
+    "url": "/mars-colonization-project/static/media/mars8.474640cc.jpg"
+  },
+  {
+    "revision": "4aee5d3f4fb904a792a297e5cbed28e1",
+    "url": "/mars-colonization-project/static/media/mars9.4aee5d3f.jpg"
+  }
+]);
